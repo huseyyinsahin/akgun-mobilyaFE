8e41654cdf0623a2b2fa6aef010efef65e99c60c
huseyyinsahin <huseysahin19@outlook.com>
2024-12-09 19:33:43 +0300

    Home

--- src/pages/user/Home.jsx
+++ src/pages/user/Home.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import { Box, Container, Grid, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import photo1 from "../../assests/images/photo1.jpg";
+import photo2 from "../../assests/images/photo2.jpg";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import Carousel from "../../components/Carousel";
+import Reference from "../../components/Reference";
 
 const Home = () => {
   return (
-    <div>
-        
-    </div>
-  )
-}
+    <>
+      <Carousel />
+      <Reference />
+    </>
+  );
+};
 
-export default Home
+export default Home;
