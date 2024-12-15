63350b0621ac9ae0d1f5f48a07b2124dbcf908e2
huseyyinsahin <huseysahin19@outlook.com>
2024-12-15 19:27:02 +0300

    About

--- src/pages/user/PhotoGallery.jsx
+++ src/pages/user/PhotoGallery.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { Box, Card, Container, Typography } from "@mui/material";
+import React from "react";
+import photo1 from "../../assests/images/photo1.jpg";
+import photo2 from "../../assests/images/photo2.jpg";
 
 const PhotoGallery = () => {
-  return (
-    <div>PhotoGallery</div>
-  )
-}
+  const mockData = [
+    { title: "Living Room Furniture", image: photo1 },
+    { title: "Bedroom Sets", image: photo2 },
+    { title: "Office Desks", image: photo1 },
+    { title: "Dining Tables", image: photo2 },
+    { title: "Outdoor Furniture", image: photo1 },
+    { title: "Storage Solutions", image: photo2 },
+    { title: "Kitchen Cabinets", image: photo1 },
+    { title: "Bathroom Vanities", image: photo2 },
+    { title: "Kids Furniture", image: photo1 },
+    { title: "Luxury Sofas", image: photo2 },
+  ];
 
-export default PhotoGallery
+  return (
+    <>
+    <Typography variant="h2" sx={{margin:"auto",marginTop:"2rem"}}>Galeri</Typography>
+    <Container sx={{display:"flex",flexWrap:"wrap",justifyContent:"space-between",marginTop:"2rem"}}>
+      
+      {mockData.map((item) => (
+        <Card >
+          <img width="350px" src={item.image} alt="" />
+          <Typography>{item.title}</Typography>
+        </Card>
+      ))}
+    </Container>
+    </>
+  );
+};
+
+export default PhotoGallery;
