54ec71fc75c28b807d122f2681f4257d20e19ab9
huseyyinsahin <huseysahin19@outlook.com>
2024-12-31 18:04:28 +0300

    projects

--- src/pages/user/MyProjects.jsx
+++ src/pages/user/MyProjects.jsx
@@ -1,6 +1,12 @@
-import { Box, Container, Grid, Pagination } from "@mui/material";
+import {
+  Alert,
+  Box,
+  Container,
+  Grid,
+  Pagination,
+  Skeleton,
+} from "@mui/material";
 import React, { useEffect, useState } from "react";
-
 import { useNavigate } from "react-router-dom";
 import useProjectsRequest from "../../hooks/useProjectsRequest";
 import { useSelector } from "react-redux";
@@ -15,7 +21,7 @@ const MyProjects = () => {
     getProjects();
   }, []);
 
-  const { fail, loading, projects, projectsPages } = useSelector(
+  const { error, loading, projects, projectsPages } = useSelector(
     (state) => state.data
   );
 
@@ -26,50 +32,81 @@ const MyProjects = () => {
   };
 
   return (
-    <Container>
-      <Grid container spacing={2} sx={{ marginTop: "2rem" }}>
-        {projects?.map((proje) => (
-          <Grid
-            key={proje._id}
-            item
-            xs={12}
-            md={6}
-            sx={{ cursor: "pointer", margin: "0.6rem 0rem" }}
-            onClick={() => navigate(`/myprojects/:${proje._id}`)}
-          >
-            <Box
-              component="img"
-              src={proje.image[0]}//* base_url
-              alt="Mobilya"
+    <>
+      {!loading && error && (
+        <Alert severity="error" sx={{ width: "80%", margin: "auto" }}>
+          Bu bilgiler yüklenemedi, şuanda bir hata var sayfayı yenileyiniz!
+        </Alert>
+      )}
+      {loading ? (
+        <Container
+          sx={{
+            display: "flex",
+            flexWrap: "wrap",
+            gap: "20px",
+            marginTop: "20px",
+          }}
+        >
+          <Skeleton variant="rectangular" width="48%" height={400} />
+          <Skeleton variant="rectangular" width="48%" height={400} />
+          <Skeleton variant="rectangular" width="48%" height={400} />
+          <Skeleton variant="rectangular" width="48%" height={400} />
+          <Skeleton variant="rectangular" width="48%" height={400} />
+          <Skeleton variant="rectangular" width="48%" height={400} />
+        </Container>
+      ) : (
+        !error && (
+          <Container>
+            <Grid container spacing={2} sx={{ marginTop: "2rem" }}>
+              {projects?.map((proje) => (
+                <Grid
+                  key={proje._id}
+                  item
+                  xs={12}
+                  md={6}
+                  sx={{ cursor: "pointer", margin: "0.6rem 0rem" }}
+                  onClick={() => navigate(`/myprojects/:${proje._id}`)}
+                >
+                  <Box
+                    component="img"
+                    src={proje.image[0]} //* base_url
+                    alt="Mobilya"
+                    sx={{
+                      width: "100%",
+                      height: { xs: "13rem", md: "17rem" },
+                      objectFit: "cover",
+                      backgroundSize: "cover",
+                    }}
+                  />
+                  <Box
+                    sx={{
+                      backgroundColor: "#2C2C2C",
+                      color: "white",
+                      textAlign: "center",
+                      fontSize: { xs: "0.8rem ", md: "1.3rem" },
+                    }}
+                    p={2}
+                  >
+                    {proje.title}
+                  </Box>
+                </Grid>
+              ))}
+            </Grid>
+            <Pagination
               sx={{
-                width: "100%",
-                height: { xs: "13rem", md: "17rem" },
-                objectFit: "cover",
-                backgroundSize: "cover",
+                display: "flex",
+                justifyContent: "center",
+                marginTop: "1.5rem",
               }}
+              onChange={handlePage}
+              page={page}
+              count={projectsPages?.total}
+              variant="outlined"
             />
-            <Box
-              sx={{
-                backgroundColor: "#2C2C2C",
-                color: "white",
-                textAlign: "center",
-                fontSize: { xs: "0.8rem ", md: "1.3rem" },
-              }}
-              p={2}
-            >
-              {proje.title}
-            </Box>
-          </Grid>
-        ))}
-      </Grid>
-      <Pagination
-        sx={{ display: "flex", justifyContent: "center", marginTop: "1.5rem" }}
-        onChange={handlePage}
-        page={page}
-        count={projectsPages?.total}
-        variant="outlined"
-      />
-    </Container>
+          </Container>
+        )
+      )}
+    </>
   );
 };
 
