19bc591f173907adaabcd46dc0a0dd6996cd7781
huseyyinsahin <huseysahin19@outlook.com>
2025-01-05 23:45:57 +0300

    Admin Navbar

--- src/components/adminComponents/AdminNavbar.jsx
+++ src/components/adminComponents/AdminNavbar.jsx
@@ -7,79 +7,83 @@ const AdminNavbar = () => {
   const { logout } = useAuthRequest();
   const navigate = useNavigate();
   return (
-    <AppBar
-      sx={{
-        height: "6rem",
-        display: "flex",
-        justifyContent: "space-around",
-        alignItems: "center",
-        flexDirection: "row",
-      }}
-    >
-      <Button
-        onClick={() => navigate("/")}
+    <>
+      <Box sx={{ height: "6.1rem" }}></Box>
+
+      <AppBar
         sx={{
-          color: "white",
-          backgroundColor: "rgb(0, 128, 255)",
-          textTransform: "capitalize",
-          "&:hover": { backgroundColor: "lightGray", color: "gray" },
-        }}
-      >
-        Siteye Dön
-      </Button>
-      <Box
-        sx={{
+          height: "6rem",
           display: "flex",
-          flexDirection: "column",
-          textAlign: "center",
+          justifyContent: "space-around",
+          alignItems: "center",
+          flexDirection: "row",
         }}
       >
-        <Typography
-          component="h2"
-          variant="h2"
+        <Button
+          onClick={() => navigate("/")}
           sx={{
-            cursor: "default",
-            fontFamily: "Lobster",
-            fontSize: { xs: "2.4rem", md: "3.0rem" },
+            color: "white",
+            backgroundColor: "rgb(0, 128, 255)",
+            textTransform: "capitalize",
+            "&:hover": { backgroundColor: "lightGray", color: "gray" },
           }}
         >
-          AKGÜN
-        </Typography>
-        <Typography
-          component="h2"
-          variant="h2"
+          Siteye Dön
+        </Button>
+        <Box
           sx={{
-            fontFamily: "Lobster",
-            fontSize: { xs: "0.9rem", md: "1rem" },
-            cursor: "default",
+            display: "flex",
+            flexDirection: "column",
+            textAlign: "center",
           }}
         >
-          Mobilya & Dekorasyon
-        </Typography>
-        <Typography
-          component="h2"
-          variant="h2"
+          <Typography
+            component="h2"
+            variant="h2"
+            sx={{
+              cursor: "default",
+              fontFamily: "Lobster",
+              fontSize: { xs: "2.4rem", md: "3.0rem" },
+            }}
+          >
+            AKGÜN
+          </Typography>
+          <Typography
+            component="h2"
+            variant="h2"
+            sx={{
+              fontFamily: "Lobster",
+              fontSize: { xs: "0.9rem", md: "1rem" },
+              cursor: "default",
+            }}
+          >
+            Mobilya & Dekorasyon
+          </Typography>
+          <Typography
+            component="h2"
+            variant="h2"
+            sx={{
+              fontSize: { xs: "0.8rem", md: "1rem" },
+              cursor: "default",
+              letterSpacing: { xs: "5px", md: "10px" },
+            }}
+          >
+            ADMİN PANELİ
+          </Typography>
+        </Box>
+        <Button
+          onClick={logout}
           sx={{
-            fontSize: { xs: "0.8rem", md: "1rem" },
-            cursor: "default",
-            letterSpacing: { xs: "5px", md: "10px" },
+            color: "white",
+            backgroundColor: "rgb(0, 128, 255)",
+            textTransform: "capitalize",
+            "&:hover": { backgroundColor: "lightGray", color: "gray" },
           }}
         >
-          ADMİN PANELİ
-        </Typography>
-      </Box>
-      <Button
-        onClick={logout}
-        sx={{
-          color: "white",
-          backgroundColor: "rgb(0, 128, 255)",
-          textTransform: "capitalize",
-          "&:hover": { backgroundColor: "lightGray", color: "gray" },
-        }}
-      >
-        Çıkış Yap
-      </Button>
-    </AppBar>
+          Çıkış Yap
+        </Button>
+      </AppBar>
+    </>
   );
 };
 
