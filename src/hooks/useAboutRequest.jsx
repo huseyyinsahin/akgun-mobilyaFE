404a1b96afb8862770bf967e2f1f32dbd3a52dfc
huseyyinsahin <huseysahin19@outlook.com>
2025-01-21 19:07:51 +0300

    useAboutRequest

--- src/hooks/useAboutRequest.jsx
+++ src/hooks/useAboutRequest.jsx
@@ -1,6 +1,7 @@
 import { useDispatch } from "react-redux";
 import useAxios from "./useAxios";
 import { fail, setAbout, start } from "../features/dataSlice";
+import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify";
 
 const useAboutRequest = () => {
   const dispatch = useDispatch();
@@ -16,7 +17,37 @@ const useAboutRequest = () => {
     }
   };
 
-  return { getAbout };
+  const postAbout = async (aboutData) => {
+    try {
+      await axiosToken.post("about", aboutData);
+      toastSuccessNotify("Hakkımızda yazısı eklendi");
+      getAbout();
+    } catch (error) {
+      toastErrorNotify("Hakkımızda yazısı ekleme başarısız!");
+    }
+  };
+
+  const updateAbout = async (updateData, id) => {
+    try {
+      await axiosToken.put(`about/${id}`, updateData);
+      toastSuccessNotify("Hakkımızda yazısı güncellendi");
+      getAbout();
+    } catch (error) {
+      toastErrorNotify("Hakkımızda yazısı güncelleme başarısız!");
+    }
+  };
+
+  const deleteAbout = async (id) => {
+    try {
+      await axiosToken.delete(`about/${id}`);
+      toastSuccessNotify("Hakkımızda yazısı silindi");
+      getAbout();
+    } catch (error) {
+      toastErrorNotify("Hakkımızda yazısı silinemedi!");
+    }
+  };
+
+  return { getAbout, postAbout, updateAbout, deleteAbout };
 };
 
 export default useAboutRequest;
