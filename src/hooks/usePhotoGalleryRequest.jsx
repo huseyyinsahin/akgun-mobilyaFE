4be8f168afaef7bfbbf717f0e7e36319e24ecc12
huseyyinsahin <huseysahin19@outlook.com>
2025-01-22 01:02:06 +0300

    photoGalleryRequest

--- src/hooks/usePhotoGalleryRequest.jsx
+++ src/hooks/usePhotoGalleryRequest.jsx
@@ -1,25 +1,66 @@
 import { useDispatch } from "react-redux";
 import useAxios from "./useAxios";
 import { fail, setPhotoGallery, start } from "../features/dataSlice";
+import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify";
 
 const usePhotoGalleryRequest = () => {
   const dispatch = useDispatch();
   const { axiosToken, axiosPublic } = useAxios();
 
-  const getPhotoGallery = async (pageCount = "1") => {
+  const getPhotoGallery = async (pageCount = "1", limit = "21") => {
     dispatch(start());
     try {
       const { data } = await axiosPublic.get(
-        `photogallery?page=${pageCount}&limit=21` 
+        `photogallery?page=${pageCount}&limit=${limit}`
       );
       dispatch(setPhotoGallery(data));
-      console.log(data);
     } catch (error) {
       dispatch(fail());
     }
   };
 
-  return { getPhotoGallery };
+  const postPhotoGallery = async (photoGalleryData, currentPage) => {
+    try {
+      await axiosToken.post("photoGallery", photoGalleryData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      toastSuccessNotify("Fotograf eklendi");
+      getPhotoGallery(currentPage, 10);
+    } catch (error) {
+      toastErrorNotify("Fotograf ekleme başarısız!");
+    }
+  };
+
+  const updatePhotoGallery = async (updateData, id, currentPage) => {
+    try {
+      await axiosToken.put(`photoGallery/${id}`, updateData, {
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+      toastSuccessNotify("Fotograf güncellendi");
+      getPhotoGallery(currentPage, 10);
+    } catch (error) {
+      toastErrorNotify("Fotograf güncelleme başarısız!");
+    }
+  };
+
+  const deletePhotoGallery = async (id, currentPage) => {
+    try {
+      await axiosToken.delete(`photoGallery/${id}`);
+      toastSuccessNotify("Fotograf silindi");
+      getPhotoGallery(currentPage, 10);
+    } catch (error) {
+      toastErrorNotify("Fotograf silinemedi!");
+    }
+  };
+
+  return {
+    getPhotoGallery,
+    postPhotoGallery,
+    deletePhotoGallery,
+    updatePhotoGallery,
+  };
 };
 
 export default usePhotoGalleryRequest;
