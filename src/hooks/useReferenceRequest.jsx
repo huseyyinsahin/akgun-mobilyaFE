f5fb2fa3e70093a1045bd5aefbe13158148220cb
huseyyinsahin <huseysahin19@outlook.com>
2025-01-21 14:53:28 +0300

    useReferenceRequest

--- src/hooks/useReferenceRequest.jsx
+++ src/hooks/useReferenceRequest.jsx
@@ -1,6 +1,7 @@
 import { useDispatch } from "react-redux";
 import useAxios from "./useAxios";
 import { fail, setReference, start } from "../features/dataSlice";
+import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify";
 
 const useReferenceRequest = () => {
   const dispatch = useDispatch();
@@ -16,7 +17,37 @@ const useReferenceRequest = () => {
     }
   };
 
-  return { getReference };
+  const postReference = async (referenceData) => {
+    try {
+      await axiosToken.post("reference", referenceData);
+      toastSuccessNotify("Referans eklendi");
+      getReference();
+    } catch (error) {
+      toastErrorNotify("Referans ekleme başarısız!");
+    }
+  };
+
+  const updateReference = async (updateData, id) => {
+    try {
+      await axiosToken.put(`reference/${id}`, updateData);
+      toastSuccessNotify("Referans güncellendi");
+      getReference();
+    } catch (error) {
+      toastErrorNotify("Referans güncelleme başarısız!");
+    }
+  };
+
+  const deleteReference = async (id) => {
+    try {
+      await axiosToken.delete(`reference/${id}`);
+      toastSuccessNotify("Referans silindi");
+      getReference();
+    } catch (error) {
+      toastErrorNotify("Referans silinemedi!");
+    }
+  };
+
+  return { getReference, postReference, updateReference, deleteReference };
 };
 
 export default useReferenceRequest;
