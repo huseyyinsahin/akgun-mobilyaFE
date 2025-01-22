d7f7a912f946850359a158807a1117e403a1b345
huseyyinsahin <huseysahin19@outlook.com>
2025-01-23 02:22:57 +0300

    useProjectRequest

--- src/features/dataSlice.jsx
+++ src/features/dataSlice.jsx
@@ -11,7 +11,7 @@ const blogSlice = createSlice({
     photoGalleryPages: [],
     projects: [],
     projectsPages: [],
-    projectDetails: [],
+    projectDetails: false,
     loading: "",
     error: "",
   },
--- src/hooks/useProjectsRequest.jsx
+++ src/hooks/useProjectsRequest.jsx
@@ -6,6 +6,7 @@ import {
   start,
   setProjectDetails,
 } from "../features/dataSlice";
+import { toastSuccessNotify, toastErrorNotify } from "../helper/ToastNotify";
 
 const useProjectsRequest = () => {
   const dispatch = useDispatch();
@@ -17,6 +18,7 @@ const useProjectsRequest = () => {
       const { data } = await axiosPublic.get(
         `projects?page=${pageCount}&limit=6`
       );
+      console.log(data);
       dispatch(setProjects(data));
     } catch (error) {
       dispatch(fail());
@@ -24,16 +26,56 @@ const useProjectsRequest = () => {
   };
 
   const readProjects = async (id) => {
-    dispatch(start());
     try {
       const { data } = await axiosPublic.get(`projects/${id}`);
       dispatch(setProjectDetails(data));
     } catch (error) {
-      dispatch(fail());
     }
   };
 
-  return { getProjects, readProjects };
+  const postProjects = async (projectsData, currentPage) => {
+    try {
+      await axiosToken.post("projects", projectsData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      toastSuccessNotify("Proje eklendi");
+      getProjects(currentPage, 6);
+    } catch (error) {
+      toastErrorNotify("Proje ekleme başarısız!");
+    }
+  };
+
+  const updateProjects = async (updateData, id, currentPage) => {
+    try {
+      await axiosToken.put(`projects/${id}`, updateData, {
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+      toastSuccessNotify("Proje güncellendi");
+      getProjects(currentPage, 6);
+    } catch (error) {
+      toastErrorNotify("Proje güncelleme başarısız!");
+    }
+  };
+
+  const deleteProjects = async (id, currentPage) => {
+    try {
+      await axiosToken.delete(`projects/${id}`);
+      toastSuccessNotify("Proje silindi");
+      getProjects(currentPage, 6);
+    } catch (error) {
+      toastErrorNotify("Proje silinemedi!");
+    }
+  };
+
+  return {
+    getProjects,
+    readProjects,
+    postProjects,
+    updateProjects,
+    deleteProjects,
+  };
 };
 
 export default useProjectsRequest;
