b4fb6d3856f8248bb4bd27790f82e5ab3523f2bc
huseyyinsahin <huseysahin19@outlook.com>
2025-01-25 02:29:31 +0300

    AdminLogin

--- src/pages/admin/AdminLogin.jsx
+++ src/pages/admin/AdminLogin.jsx
@@ -1,12 +1,20 @@
 import { Form, Formik } from "formik";
 import useAuthRequest from "../../hooks/useAuthRequest";
-import { Button, TextField, Typography, Box, Container } from "@mui/material";
+import {
+  Button,
+  TextField,
+  Typography,
+  Box,
+  Container,
+  CircularProgress,
+} from "@mui/material";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import { object, string } from "yup";
+import { useSelector } from "react-redux";
 
 const AdminLogin = () => {
-  //* loading
   const { login } = useAuthRequest();
+  const { loading } = useSelector((state) => state.auth);
 
   const loginSchema = object({
     username: string()
@@ -28,129 +36,150 @@ const AdminLogin = () => {
 
   return (
     <>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-          backgroundColor: "rgb(234, 234, 234)",
-          padding: { xs: "20px", md: "0px" },
-        }}
-      >
-        <Container
-          maxWidth="xs"
+      {loading ? (
+        <Box
           sx={{
-            padding: { xs: "30px", md: "20px", xl: "60px 20px" },
-            borderRadius: "12px",
-            boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.5)",
-            backgroundColor: "white",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+            background:
+              "linear-gradient(145deg,rgb(139, 123, 100), #A8906C, rgb(139, 123, 100))",
           }}
         >
-          <Formik
-            initialValues={{ username: "", password: "" }}
-            validationSchema={loginSchema}
-            onSubmit={(values, actions) => {
-              login(values);
-              actions.resetForm();
-              actions.setSubmitting(false);
+          <CircularProgress color="black" size={150} />
+        </Box>
+      ) : (
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+            background:
+              "linear-gradient(145deg,rgb(139, 123, 100), #A8906C, rgb(139, 123, 100))",
+            padding: { xs: "20px", md: "0px" },
+          }}
+        >
+          <Container
+            maxWidth="sm"
+            sx={{
+              padding: { xs: "30px", md: "20px", xl: "60px 20px" },
+              borderRadius: "12px",
+              boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.5)",
+              backgroundColor: "white",
             }}
           >
-            {({
-              isSubmitting,
-              handleChange,
-              handleBlur,
-              values,
-              touched,
-              errors,
-              isValid,
-            }) => (
-              <Form>
-                <Box
-                  sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    textAlign: "center",
-                  }}
-                >
-                  <Typography
-                    variant="h2"
-                    component="h2"
+            <Formik
+              initialValues={{ username: "", password: "" }}
+              validationSchema={loginSchema}
+              onSubmit={(values, actions) => {
+                login(values);
+                actions.resetForm();
+                actions.setSubmitting(false);
+              }}
+            >
+              {({
+                isSubmitting,
+                handleChange,
+                handleBlur,
+                values,
+                touched,
+                errors,
+                isValid,
+              }) => (
+                <Form>
+                  <Box
                     sx={{
-                      cursor: "default",
-                      fontFamily: "Lobster",
-                      fontSize: { xs: "2.8rem", md: "3.5rem" },
+                      display: "flex",
+                      flexDirection: "column",
+                      textAlign: "center",
                     }}
                   >
-                    AKGÜN
-                  </Typography>
+                    <Typography
+                      variant="h2"
+                      component="h2"
+                      sx={{
+                        cursor: "default",
+                        fontFamily: "Lobster",
+                        fontSize: { xs: "2.8rem", md: "3.5rem" },
+                        background:
+                          "linear-gradient(145deg, #7E643C, #A8906C, #D7C4A5)",
+                        WebkitBackgroundClip: "text",
+                        WebkitTextFillColor: "transparent",
+                      }}
+                    >
+                      AKGÜN
+                    </Typography>
+                    <Typography
+                      variant="h2"
+                      component="h2"
+                      sx={{
+                        fontFamily: "Lobster",
+                        fontSize: { xs: "01rem", md: "1.2rem" },
+                        cursor: "default",
+                        background:
+                          "linear-gradient(145deg, #7E643C, #A8906C, #D7C4A5)",
+                        WebkitBackgroundClip: "text",
+                        WebkitTextFillColor: "transparent",
+                      }}
+                    >
+                      Mobilya & Dekorasyon
+                    </Typography>
+                  </Box>
+                  <TextField
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                    label="Kullanıcı Adı"
+                    type="text"
+                    name="username"
+                    onChange={handleChange}
+                    value={values.username}
+                    error={touched.username && Boolean(errors.username)}
+                    onBlur={handleBlur}
+                    helperText={errors.username}
+                  />
+                  <TextField
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                    label="Şifre"
+                    type="password"
+                    name="password"
+                    onChange={handleChange}
+                    value={values.password}
+                    error={touched.password && Boolean(errors.password)}
+                    onBlur={handleBlur}
+                    helperText={errors.password}
+                  />
                   <Typography
-                    variant="h2"
-                    component="h2"
+                    sx={{ textAlign: "center", color: " #A8906C", mt: 1 }}
+                  >
+                    {Object.keys(touched).length > 0 && isValid && (
+                      <CheckCircleIcon />
+                    )}
+                  </Typography>
+                  <Button
+                    type="submit"
+                    disabled={isSubmitting}
+                    fullWidth
+                    variant="contained"
                     sx={{
-                      fontFamily: "Lobster",
-                      fontSize: { xs: "01rem", md: "1.2rem" },
-                      cursor: "default",
+                      mb: 2,
+                      mt: 2,
+                      backgroundColor: "#A8906C",
+                      color: "#fff",
                     }}
                   >
-                    Mobilya & Dekorasyon
-                  </Typography>
-                </Box>
-                <TextField
-                  variant="outlined"
-                  margin="normal"
-                  fullWidth
-                  label="Kullanıcı Adı"
-                  type="text"
-                  name="username"
-                  onChange={handleChange}
-                  value={values.username}
-                  error={touched.username && Boolean(errors.username)}
-                  onBlur={handleBlur}
-                  helperText={errors.username}
-                />
-                <TextField
-                  variant="outlined"
-                  margin="normal"
-                  fullWidth
-                  label="Şifre"
-                  type="password"
-                  name="password"
-                  onChange={handleChange}
-                  value={values.password}
-                  error={touched.password && Boolean(errors.password)}
-                  onBlur={handleBlur}
-                  helperText={errors.password}
-                />
-                <Typography
-                  sx={{ textAlign: "center", color: " #0288d1", mt: 1 }}
-                >
-                  {Object.keys(touched).length > 0 && isValid && (
-                    <CheckCircleIcon />
-                  )}
-                </Typography>
-                <Button
-                  type="submit"
-                  disabled={isSubmitting}
-                  fullWidth
-                  variant="contained"
-                  sx={{
-                    mb: 2,
-                    mt: 2,
-                    backgroundColor: "#2C2C2C",
-                    color: "#fff",
-                    "&:hover": {
-                      backgroundColor: "black",
-                    },
-                  }}
-                >
-                  Giriş Yap
-                </Button>
-              </Form>
-            )}
-          </Formik>
-        </Container>
-      </Box>
+                    Giriş Yap
+                  </Button>
+                </Form>
+              )}
+            </Formik>
+          </Container>
+        </Box>
+      )}
     </>
   );
 };
